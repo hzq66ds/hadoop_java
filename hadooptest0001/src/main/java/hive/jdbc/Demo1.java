c798e9f7088460e6e32acd9ebc845b006f4720ee
韩志强 <x_hzq@sina.com>
2019-07-05 21:05:02 +0800

    hive连接数据库

--- hadooptest0001/src/main/java/hive/jdbc/Demo1.java
+++ hadooptest0001/src/main/java/hive/jdbc/Demo1.java
@@ -10,37 +10,47 @@ public class Demo1 {
 			e.printStackTrace();
 			System.exit(1);
 		}
-		Connection con = DriverManager.getConnection("jdbc:hive2://172.16.1.240:10000/default", "root", "root");
-		Statement stmt = con.createStatement();
+		Connection con = DriverManager.getConnection("jdbc:hive2://hadoopmaster:10000/default", "root", "root");
+
 
 		String sql = "show tables";
 		System.out.println("Running: " + sql);
-		ResultSet res = stmt.executeQuery(sql);
-		if (res.next()) {
-			System.out.println(res.getString(1));
-		}
-
-		// describe table
-		sql = "describe teacherexternal";
-		System.out.println("Running: " + sql);
-		res = stmt.executeQuery(sql);
+		Statement stmt3 = con.createStatement();
+		ResultSet res = stmt3.executeQuery(sql);
 		while (res.next()) {
-			System.out.println(res.getString(1) + "\t" + res.getString(2));
-		}
+			System.out.println("-------表："+res.getString(1));
+			// describe table
+			String tablename = res.getString(1);
+			sql = "describe "+tablename;
+			System.out.println("Running: " + sql);
+			Statement stmt2 = con.createStatement();
+			ResultSet res1 = stmt2.executeQuery(sql);
+			while (res1.next()) {
+				System.out.println(res1.getString(1) + "\t" + res1.getString(2));
+			}
 
 
-		sql = "select * from teacherexternal";
-		System.out.println("Running: " + sql);
-		res = stmt.executeQuery(sql);
-		while (res.next()) {
-			System.out.println(String.valueOf(res.getObject(1)) + "\t" + res.getString(2)+ "\t" + res.getInt(3)+ "\t" + res.getString(4));
-		}
+			sql = "select * from "+tablename;
+			System.out.println("Running: " + sql);
+			Statement stmt1 = con.createStatement();
+			ResultSet res2 = stmt1.executeQuery(sql);
+			while (res2.next()) {
+				ResultSetMetaData rsmd = res2.getMetaData() ;
+				int columnCount = rsmd.getColumnCount();
+				StringBuffer buffer = new StringBuffer();
+				for (int i=1;i<=columnCount;i++){
+					buffer.append(res2.getObject(i)).append("\t");
+				}
+				System.out.println(buffer.toString());
+			}
 
-		sql = "select count(1) from teacherexternal";
-		System.out.println("Running: " + sql);
-		res = stmt.executeQuery(sql);
-		while (res.next()) {
-			System.out.println(res.getString(1));
+			sql = "select count(1) from "+tablename;
+			System.out.println("Running: " + sql);
+			Statement stmt = con.createStatement();
+			ResultSet res3 = stmt.executeQuery(sql);
+			while (res3.next()) {
+				System.out.println(res3.getString(1));
+			}
 		}
 	}
 }
